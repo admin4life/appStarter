10f00350d8a090fd80d3ebbdf3c45a9c7b672bf7
Ian Hoeffner <admin4life@gmail.com>
2016-09-28 11:22:44 -0500

    minor updates

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.render('index.pug', {title: "Page Title", headline: "This Headline"})
 })
+app.get('/Home', function(req,res){
+  res.redirect('/');
+});
 
 app.listen(9999);
